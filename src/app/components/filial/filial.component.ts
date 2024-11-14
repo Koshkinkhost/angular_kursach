a369239d5363d5aa67c1a4cc78ab1727d93ffd0a
Борис <102375597+Koshkinkhost@users.noreply.github.com>
2024-11-14 17:26:41 +0300

    14.11 commit

--- src/app/components/filial/filial.component.ts
+++ src/app/components/filial/filial.component.ts
@@ -10,7 +10,7 @@ import { Component,Input } from '@angular/core';
 export class FilialComponent {
   private geocoderUrl = 'https://geocode-maps.yandex.ru/1.x/';
   private staticApiUrl = 'https://static-maps.yandex.ru/1.x/';
-  private key: string = '74ff6712-3c2f-4384-8eaa-89e9b194ac6e';
+  private key: string = '1ad87012-0d6f-4c6a-b023-1f5159b8fc5b';
   public source:string='';
   @Input() title!:string;
   @Input() phone_num!:string;
@@ -22,28 +22,30 @@ export class FilialComponent {
   public info:string='На карте'
   async getLocation() {
     try {
-      const response = await fetch(`${this.geocoderUrl}?apikey=${this.key}&geocode=${this.street},+${this.build}&format=json`);
+      const response = await fetch(
+        `${this.geocoderUrl}?apikey=${this.key}&geocode=${encodeURIComponent(this.street + ',' + this.build)}&format=json`
+      );
+  
+      if (!response.ok) {
+        throw new Error(`Ошибка ${response.status}: ${response.statusText}`);
+      }
+  
       const data = await response.json();
-
+      console.log(data);
+  
       // Извлекаем координаты из ответа
       const geoObjects = data.response.GeoObjectCollection.featureMember;
       if (geoObjects.length > 0) {
-        const coordinates = geoObjects[0].GeoObject.Point.pos.split(' ').reverse(); // Координаты в формате "долгота, широта"
+        const coordinates = geoObjects[0].GeoObject.Point.pos.split(' ').reverse(); // "долгота, широта"
         const [latitude, longitude] = coordinates;
-
+  
         // Создаем URL для статической карты
         const mapUrl = `${this.staticApiUrl}?ll=${longitude},${latitude}&size=450,450&z=16&l=map&pt=${longitude},${latitude},pm2rdm`;
         console.log('URL статической карты:', mapUrl);
-
-        // Отображаем карту на странице
-        const mapImage = document.createElement('img');
+  
         this.source = mapUrl;
-        this.visible=!this.visible;
-        if(this.visible)
-        this.info="Скрыть"
-      else
-      this.info="На карте"
-       
+        this.visible = !this.visible;
+        this.info = this.visible ? 'Скрыть' : 'На карте';
       } else {
         console.error('Адрес не найден');
       }
@@ -51,4 +53,5 @@ export class FilialComponent {
       console.error('Ошибка при получении данных:', error);
     }
   }
+  
 }
