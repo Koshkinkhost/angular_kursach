dbe8639bdf9e8d6ba1100b3a1c3e811d03f075e0
Борис <102375597+Koshkinkhost@users.noreply.github.com>
2024-12-18 12:45:18 +0300

    commit 18.12

--- src/app/components/account/account.component.ts
+++ src/app/components/account/account.component.ts
@@ -4,6 +4,7 @@ import { NgForm } from '@angular/forms';
 import { FormsModule } from '@angular/forms';
 import { TracksComponent } from '../tracks/tracks.component';
 import { ArtistBioComponent } from '../../artist-bio/artist-bio.component';
+import { RegistrationService } from './RegistrationService';
 
 @Component({
   selector: 'app-account',
@@ -22,29 +23,43 @@ export class AccountComponent {
   confirm_password: string = '';
   yt_result: string[] = [];
   users: Users[] = [];
+constructor(private registr:RegistrationService){
 
-  ngOnInit() {
-    const savedUsers = localStorage.getItem('users');
-    this.users = savedUsers ? JSON.parse(savedUsers) : [];
-    console.log(this.users);
+}
+async ngOnInit() {
+  const isAuthenticated = await this.registr.CheckAuthentication();
+  if (isAuthenticated) {
+    this.access = true;
+    // Вы можете также получить данные пользователя, например:
+    this.name = 'Имя пользователя'; // Подгрузите реальное имя с сервера, если требуется
+  } else {
+    this.access = false;
   }
+}
 
   async changeBlock(id: number) {
     this.content = id;
     console.log("переход");
   }
 
-  Log() {
-    const user = this.users.find(user => user.login === this.us_log && user.password === this.us_password);
-    if (user) {
-      alert("вы зашли");
-      this.name = this.us_log;
-      this.us_log = '';
-      this.us_password = '';
-      this.access = true;
-      return;
+  async Log() {
+const r=await this.registr.Login(this.us_log,this.us_password);
+
+    if(r.success){
+      this.access=true
+      
+        this.users.push({login:this.us_log,password:this.us_password})
+        this.name=this.us_log;
+        
+        console.log("добавил пользователя в список")
+      
     }
-    alert("такого пользователя нет");
+    else{
+      this.access=false;
+    }
+  }
+  Logout(){
+    
   }
 
   change() {
@@ -54,20 +69,12 @@ export class AccountComponent {
     this.isLogin = !this.isLogin;
   }
 
-  Registration() {
-    if (this.confirm_password === this.us_password) {
-        let keys = Object.keys(localStorage);
-        if (!keys.includes(this.us_log)) {
-            this.users.push({ login: this.us_log, password: this.us_password });
-            localStorage.setItem('users', JSON.stringify(this.users));
-        }
-        console.log(localStorage.getItem('users'));
-        this.isLogin = true;
-        this.us_log = '';
-        this.us_password = '';
-        this.confirm_password = ''; // Clear confirm_password after registration
-    } else {
-        alert("пароли не совпадают");
+  async Registration() {
+   const data=await this.registr.Registration(this.us_log,this.us_password,this.confirm_password)
+    if(data){
+      alert("вы зарегистрировались");
     }
+    
+    
 }
 }
--- src/app/components/card/card.component.ts
+++ src/app/components/card/card.component.ts
@@ -1,6 +1,7 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 import { Artist } from '../Artist';
 import { LastFmService } from '../../last-fm.service';
+
 @Component({
   selector: 'app-card',
   standalone: true,
@@ -8,50 +9,37 @@ import { LastFmService } from '../../last-fm.service';
   templateUrl: './card.component.html',
   styleUrl: './card.component.css'
 })
-export class CardComponent {
+export class CardComponent implements OnInit {
   @Input() artist!: Artist;
   photo: string = "";
-  images: { name: string, url: string }[] = []
-  selected?: string = ""
-  access_key: string = "gRZ2pWZY-MFYQ1tUVnG_Gb7u4ZnabITvRj6jPf7OVuI"
-  url: string = `https://api.unsplash.com/search/photos?query=Noize MC`
-  constructor(private fm: LastFmService) {
+  images: { name: string, url: string }[] = [];
+  selected?: string = "";
 
-  }
+  constructor(private fm: LastFmService) {}
+
   async ngOnInit() {
-    const caucheImages = localStorage.getItem("artistImages");
-    if (caucheImages) {
-      this.images = JSON.parse(caucheImages)
-      const d = this.images.find(u => u.name === this.artist.name)
-      if (!d) {
+    const cachedImages = localStorage.getItem("artistImages");
+    if (cachedImages) {
+      this.images = JSON.parse(cachedImages);
+    }
+
+    const cachedImage = this.images.find(u => u.name === this.artist.name);
+    if (cachedImage) {
+      this.selected = cachedImage.url;
+      console.log("Изображение загружено из кеша");
+    } else {
+      try {
         const data = await this.fm.find_photo(this.artist.name);
         this.photo = data.items[0].link;
         this.selected = this.photo;
-        this.images.push({ name: this.artist.name, url: this.selected })
-        localStorage.setItem("artistImages", JSON.stringify(this.images))
-
-
+        this.images.push({ name: this.artist.name, url: this.selected });
+        localStorage.setItem("artistImages", JSON.stringify(this.images));
+        console.log("Изображение загружено и сохранено в кеш");
+      } catch (error) {
+        console.error("Ошибка при загрузке изображения:", error);
       }
-      else {
-        const caucheImages = localStorage.getItem("artistImages");
-        const cachedImage = this.images.find(u => u.name === this.artist.name);
-        this.selected = cachedImage?.url;
-      }
-      console.log(this.images.length + "артисты");
-
-
-
-
-
-
-
     }
 
-
-
-
-
-
-
+    console.log(`Количество артистов в кеше: ${this.images.length}`);
   }
 }
